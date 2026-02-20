db0a2170f560970b65af53c5cc11eb5b8e35f0e6
dolvin17 <huertas876@gmail.com>
2026-02-20 19:46:17 +0100

    Cambio en main para provocar conflicto (borro comentarios)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         ExpenseManager myManager = new ExpenseManager();
-        // Pruebo con 50 pavos
         myManager.addExpense(50.0);
         System.out.println("Gasto de 50 añadido. Total: " + myManager.getTotalBalance());
-        // Pruebo con un número negativo
         myManager.addExpense(-10.0);
         System.out.println("Intento añadir -10. Total sigue siendo: " + myManager.getTotalBalance());
     }
