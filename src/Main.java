5eebf92338a09f9ea378f4e2e8495dda76f83450
dolvin17 <huertas876@gmail.com>
2026-02-20 19:45:02 +0100

    adding some comments

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         ExpenseManager myManager = new ExpenseManager();
-        // Pruebo con 50 pavos
-        myManager.addExpense(50.0);
-        System.out.println("Gasto de 50 añadido. Total: " + myManager.getTotalBalance());
+        // Pruebo con 200
+        myManager.addExpense(200.0);
+        System.out.println("Gasto de 200 añadido. Total: " + myManager.getTotalBalance());
         // Pruebo con un número negativo
         myManager.addExpense(-10.0);
         System.out.println("Intento añadir -10. Total sigue siendo: " + myManager.getTotalBalance());
